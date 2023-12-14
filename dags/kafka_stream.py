6cd27453b60d49db2f752fee2b20c5679292b44a
Caio Henrique Paulino <caio.paulino@pmitaqua.sp.gov.br>
2023-12-14 13:47:16 -0300

    Iniciando Kafka Producer, criando tópico users_created

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -7,6 +7,7 @@ default_args = {
     'start_date': datetime(2023, 11, 30, 10, 00)
 }
 
+
 def get_data():
     import requests
 
@@ -16,6 +17,7 @@ def get_data():
 
     return response
 
+
 def format_data(response):
     location = response['location']
 
@@ -23,7 +25,8 @@ def format_data(response):
     data['first_name'] = response['name']['first']
     data['last_name'] = response['name']['last']
     data['gender'] = response['gender']
-    data['address'] = f"{str(location['street']['number'])} {location['street']['name']} {location['city']}, {location['state']}, {location['country']}"
+    data[
+        'address'] = f"{str(location['street']['number'])} {location['street']['name']} {location['city']}, {location['state']}, {location['country']}"
     data['postcode'] = location['postcode']
     data['email'] = response['email']
     data['username'] = response['login']['username']
@@ -34,22 +37,28 @@ def format_data(response):
 
     return data
 
+
 def stream_data():
     import json
+    from kafka import KafkaProducer
+    import time
 
     response = get_data()
     response = format_data(response)
-    print(json.dumps(response, indent=3))
+    # print(json.dumps(response, indent=3))
+
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'], max_block_ms=5000)
+
+    producer.send('users_created', json.dumps(response).encode('utf-8'))
 
 
-with DAG('user_automation',
-         default_args = default_args,
-         schedule = '@daily',
-         catchup = False) as dag:
+# with DAG('user_automation',
+#          default_args=default_args,
+#          schedule='@daily',
+#          catchup=False) as dag:
+#     streaming_task = PythonOperator(
+#         task_id='stream_data_from_api',
+#         python_callable=stream_data
+#     )
 
-    streaming_task = PythonOperator(
-        task_id = 'stream_data_from_api',
-        python_callable = stream_data
-    )
-
-stream_data();
+# stream_data();
